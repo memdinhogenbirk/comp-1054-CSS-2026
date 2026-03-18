96464d8ccc4e9ecdd5582ae319a4dac5820bda2b
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-03-18 13:41:58 -0400

    replacing visible with active, makes more sense IMO

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -6,19 +6,19 @@ tabs.forEach((tab) =>{
     tab.addEventListener('click', ()=>{
         //when any tab is clicked
         //begin by removing class visbile from all elements
-        tabs.forEach((t) =>{t.classList.remove('visible')});
-        panels.forEach((p) =>{p.classList.remove('visible')});
-        //add visible to tab that has been clicked
-        tab.classList.add('visible');
+        tabs.forEach((t) =>{t.classList.remove('active')});
+        panels.forEach((p) =>{p.classList.remove('active')});
+        //add active to tab that has been clicked
+        tab.classList.add('active');
         //get the text written in clicked tabs href attribute
         currentTab = tab.getAttribute('href');
         //go through all panels
         panels.forEach((panel) =>{
             /*if the resulting string from concatonating # and the panel id is
-            equal to href text, add class visible*/
+            equal to href text, add class active*/
             if(currentTab === '#' + panel.id){
-                //add class visible to it
-                panel.classList.add('visible');
+                //add class active to it
+                panel.classList.add('active');
             };
             /*
             Alternatively, whilst trying to figure out how to do this within the 
