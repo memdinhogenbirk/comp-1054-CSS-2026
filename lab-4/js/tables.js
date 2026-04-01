ac39f063dd944b2b6ee6018293817750228caf2c
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-04-01 14:15:48 -0400

    me thinks im done

--- lab-4/js/tables.js
+++ lab-4/js/tables.js
@@ -2,4 +2,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const consoleTable = document.querySelector('table');
     let sortableTable = new Tablesort(consoleTable);
+    /*my own custom javascript to color backgrounds of the selected column when sorted*/
+    const columnHeaders = consoleTable.querySelectorAll('th');
+    const columns = consoleTable.querySelectorAll('colgroup col');
+    columnHeaders.forEach(header => {
+        header.addEventListener('click', function() {
+            /*cell index super helpful, get's index of the header cell in the row of headers
+            Thank you mozilla https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableCellElement/cellIndex*/
+            currentIndex = header.cellIndex;
+            columns.forEach((col, index) => {
+                if (index === currentIndex) {
+                    col.classList.add('active');
+                } else {
+                    col.classList.remove('active');
+                }
+            });
+        });
+    });
 });
